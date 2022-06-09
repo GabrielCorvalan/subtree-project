b5e2b69948e268d8a9a194575371a477f8a3cc9b
Gabriel Corvalan <gabrielcorvalan@MacBook-Pro-de-Gabriel.local>
2022-06-09 19:35:28 -0300

    subtree library added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <subtree-library-sample :initCounter="30" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SubtreeLibrarySample from 'subtree-library/src/lib-components/subtree-library-sample.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    SubtreeLibrarySample
   }
 }
 </script>
